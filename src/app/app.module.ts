7c3a0646b1bc76590db9c46e5a20d46c180b3fb3
santos <santos.sanchez@bue.edu.ar>
2023-03-08 21:34:14 -0300

    directivas databinding routing

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { PruebaComponent } from './components/prueba/prueba.component';
+import { ProjectComponent } from './components/project/project.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    PruebaComponent,
+    ProjectComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule, //para databinding
+    HttpClientModule //para servicio httpclient
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
